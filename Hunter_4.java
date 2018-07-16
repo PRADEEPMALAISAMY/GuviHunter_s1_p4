2c64e6ccec164c16a3c770bf1be2c98e173fb7aa
Pradeep <30965248+PRADEEPMALAISAMY@users.noreply.github.com>
2018-07-16 15:28:29 +0530

    Add files via upload

---
+++ Hunter_4.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Hunter_4 {
+public static void main(String[] args) {
+	
+
+	Scanner sc = new Scanner(System.in);
+	int ak = sc.nextInt();
+	Integer arr[] = new Integer[ak];
+	for (int i = 0; i < arr.length; i++) {
+
+		arr[i] = sc.nextInt();
+	}
+	int a=0;
+	for(int i=0;i<arr.length;i++) {
+		int count=0;
+		for(int j=0;j<arr.length;j++) {
+			if(arr[i]==arr[j]) {
+				count++;
+			}
+						
+		}
+		if(count==1) {
+			a=arr[i];
+		}
+	}
+	
+	
+	System.out.println(a);
+
+   }
+}
